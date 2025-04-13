bf5cdb74729cff48e3774445823d752ff6f3a289
Md. Udoy Hasan <udoyudoy111@gmail.com>
2025-04-14 05:27:12 +0600

    Fix nextjs logout cors RPC error

--- src/components/layout/sidebar.js
+++ src/components/layout/sidebar.js
@@ -29,13 +29,27 @@ export default function Sidebar() {
   }, [toggle]);
 
   return (
-    <div className={`${toggle ? "" : ""}min-w-[300px] h-[100vh] hidden md:block`}>
+    <div
+      className={`${toggle ? "" : ""}min-w-[300px] h-[100vh] hidden md:block`}
+    >
       <div className="flex flex-col h-full">
         <div className="flex-initial flex items-center justify-center py-5">
           <Link href="/" className="py-1.5">
-            <Image src="/images/brand/logo-t-b.png" alt="logo" width={187} height={35} className="dark:hidden block" />
+            <Image
+              src="/images/brand/logo-t-b.png"
+              alt="logo"
+              width={187}
+              height={35}
+              className="dark:hidden block"
+            />
 
-            <Image src="/images/brand/logo-t-w.png" alt="logo" width={187} height={35} className="dark:block hidden" />
+            <Image
+              src="/images/brand/logo-t-w.png"
+              alt="logo"
+              width={187}
+              height={35}
+              className="dark:block hidden"
+            />
           </Link>
         </div>
         <div className="flex-auto overflow-x-hidden overflow-y-auto">
@@ -50,14 +64,26 @@ export default function Sidebar() {
                     `}
                 >
                   {item.dropdown.length === 0 ? (
-                    <Link href={item.href} className="flex items-center p-3 text-gray-900 dark:text-white">
+                    <Link
+                      href={item.href}
+                      className="flex items-center p-3 text-gray-900 dark:text-white"
+                    >
                       <span className="flex-initial">
                         <Icon icon={item.icon} className="w-5 h-5" />
                       </span>
-                      <span className={`flex-auto ${item.dropdown.length == 0 ? "pl-3" : "px-3"}`}>{item.name}</span>
+                      <span
+                        className={`flex-auto ${
+                          item.dropdown.length == 0 ? "pl-3" : "px-3"
+                        }`}
+                      >
+                        {item.name}
+                      </span>
                       {item.dropdown.length > 0 && (
                         <span className="flex-initial">
-                          <Icon icon="solar:alt-arrow-down-broken" className="w-6 h-6" />
+                          <Icon
+                            icon="solar:alt-arrow-down-broken"
+                            className="w-6 h-6"
+                          />
                         </span>
                       )}
                     </Link>
@@ -69,7 +95,11 @@ export default function Sidebar() {
                       <span className="flex-initial">
                         <Icon icon={item.icon} className="w-5 h-5" />
                       </span>
-                      <span className={`flex-auto text-left ${item.dropdown.length == 0 ? "pl-3" : "px-3"}`}>
+                      <span
+                        className={`flex-auto text-left ${
+                          item.dropdown.length == 0 ? "pl-3" : "px-3"
+                        }`}
+                      >
                         {item.name}
                       </span>
                       <span className="flex-auto"></span>
@@ -92,14 +122,23 @@ export default function Sidebar() {
                         <li
                           key={subIndex}
                           className={`flex-initial px-2 py-1 hover:bg-gray-200 hover:dark:bg-white/10 rounded-xl my-1 font-semibold
-                            ${path === subItem.href ? "bg-gray-200 dark:bg-white/10" : ""}
+                            ${
+                              path === subItem.href
+                                ? "bg-gray-200 dark:bg-white/10"
+                                : ""
+                            }
                             `}
                         >
-                          <Link href={subItem.href} className="flex items-center p-3 text-gray-900 dark:text-white">
+                          <Link
+                            href={subItem.href}
+                            className="flex items-center p-3 text-gray-900 dark:text-white"
+                          >
                             <span className="flex-initial">
                               <Icon icon={subItem.icon} className="w-5 h-5" />
                             </span>
-                            <span className="flex-auto pl-3">{subItem.name}</span>
+                            <span className="flex-auto pl-3">
+                              {subItem.name}
+                            </span>
                           </Link>
                         </li>
                       ))}
@@ -122,7 +161,10 @@ export default function Sidebar() {
                     className="flex items-center p-3 text-gray-900 dark:text-white"
                   >
                     <span className="flex-initial">
-                      <Icon icon="solar:help-bold-duotone" className="w-5 h-5" />
+                      <Icon
+                        icon="solar:help-bold-duotone"
+                        className="w-5 h-5"
+                      />
                     </span>
                     <span className={`flex-auto pl-3`}>Help & Support</span>
                   </Link>
@@ -130,12 +172,23 @@ export default function Sidebar() {
                 <li
                   className={`flex-initial px-2 py-1 hover:bg-gray-100 dark:hover:bg-white/10 rounded-xl my-1 font-semibold`}
                 >
-                  <Link href="/auth/logout" className="flex items-center p-3 text-gray-900 dark:text-white">
+                  {/* <Link href="/auth/logout" className="flex items-center p-3 text-gray-900 dark:text-white">
                     <span className="flex-initial">
                       <Icon icon="solar:logout-3-bold-duotone" className="w-5 h-5" />
                     </span>
                     <span className={`flex-auto pl-3`}>Logout</span>
-                  </Link>
+                  </Link> */}
+                  <a href="/auth/logout" className="">
+                    <div className="flex items-center p-3 text-gray-900 dark:text-white">
+                      <span className="flex-initial">
+                        <Icon
+                          icon="solar:logout-3-bold-duotone"
+                          className="w-5 h-5"
+                        />
+                      </span>
+                      <span className={`flex-auto pl-3`}>Logout</span>
+                    </div>
+                  </a>
                 </li>
               </ul>
             </nav>
